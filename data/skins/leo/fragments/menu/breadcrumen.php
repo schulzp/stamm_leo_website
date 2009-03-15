2dac899859c73f8d2af62281bfb5d129b70c167d
pelzrutsche <pelzrutsche@web.de>
2009-03-15 12:50:01 +0100

    added parameters for menu builder

--- data/skins/leo/fragments/menu/breadcrumen.php
+++ data/skins/leo/fragments/menu/breadcrumen.php
@@ -1,5 +1,5 @@
 <?php 
 $page = $Core->SelectObj($data, $Core->GetVar($_GET, 'pid', DEFAULT_PAGE));
 
-echo MenuHelper::GetBuilder($data, $page)->getBreadcrumenHTML($page->menu, ' &gt; ');
+echo MenuHelper::GetBuilder($data, $page)->getBreadcrumenHTML($page->menu, $params);
 ?>
